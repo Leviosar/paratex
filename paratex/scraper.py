05d2b2440b251c2cb61a8a1e2463bdfd8a55fec3
gabriel <gabrielfranksimonetto@gmail.com>
2019-09-28 16:48:14 -0300

    refactoring done

--- paratex/scraper.py
+++ paratex/scraper.py
@@ -3,11 +3,29 @@ import requests
 import bs4
 from bs4 import BeautifulSoup
 
-def main():
-    URL = 'http://transparencia.alesc.sc.gov.br/presenca_plenaria_detalhes.php?id=1783'
+def get_sessions_on_month(URL:str):
+    # URL = 'http://transparencia.alesc.sc.gov.br/presenca_plenaria.php'
+    html = load_html(URL)
+    soup = BeautifulSoup(html, 'html.parser')
+
+    attendances = (soup.find('table', {'summary': "Presença dos Deputados"})
+                        .findAll('tr', {'style': "text-align: center;"}))
+
+    date_href_tuples = []
+    for tr in attendances:
+        tds = tr.findAll('td')
+        date = tds[2].text
+        href = tds[3].find('a').attrs['href']
+        date_href_tuples.append((date, href))
+
+
+def get_attendance(URL: str) -> pd.DataFrame:
+    # URL = 'http://transparencia.alesc.sc.gov.br/presenca_plenaria_detalhes.php?id=1783'
     html = load_html(URL)
     one_day_presences = parse(html)
-    visualizing_results(one_day_presences)
+    df = make_df(one_day_presences)
+    print(df.tail())
+    return df
 
 
 def load_html(url: str) -> str:
@@ -39,13 +57,7 @@ def build_one_observation(tr: bs4.element.Tag, date:str) -> (str, str, str, str)
     return (name, presence, justification, date)
 
 
-# Intended to be used later on the code
-# will leave this here for utility purposes for now
-def visualizing_results(observations):
+def make_df(observations):
     df = pd.DataFrame(observations)
     df = df.rename({0:'Nome', 1:'Presenca', 2:'Justificativa', 3:'Data'}, axis=1)
-    print(df.tail())
-
-
-if __name__ == '__main__':
-    main()
+    return df
